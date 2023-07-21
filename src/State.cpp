4496eb78eae9dd3cf0fd5e4d858d1b40835766c5
Paulo Alvim <pauloalvim@gmail.com>
2023-07-21 09:32:54 -0300

    New state interface.

---
+++ src/State.cpp
@@ -0,0 +1,69 @@
+#include "State.hpp"
+
+State::State()
+{
+    this->quitRequested = false;
+    this->popRequested = false;
+    this->started = false;
+}
+
+State::~State()
+{
+    objectArray.clear();
+}
+
+std::weak_ptr<GameObject> State::AddObject(GameObject *object)
+{
+    objectArray.emplace_back(object);
+
+    if (started)
+    {
+        object->Start();
+    }
+
+    return GetObjectPtr(object);
+}
+std::weak_ptr<GameObject> State::GetObjectPtr(GameObject *object)
+{
+    for (auto &it : objectArray)
+    {
+        if ((it).get() == object)
+        {
+            return std::weak_ptr<GameObject>(it);
+        }
+    }
+
+    return std::weak_ptr<GameObject>();
+}
+
+bool State::PopRequested(){
+    return this->popRequested;
+}
+
+bool State::QuitRequested(){
+    return this->quitRequested;
+}
+
+void State::RenderArray(){
+    for (size_t i = 0; i < objectArray.size(); i++)
+    {
+        objectArray[i]->Render();
+    }
+    
+}
+
+void State::UpdateArray(float dt){
+    for (size_t i = 0; i < objectArray.size(); i++)
+    {
+        objectArray[i]->Update(dt);
+    }
+    
+}
+
+void State::StartArray(){
+    for (size_t i = 0; i < objectArray.size(); i++)
+    {
+        objectArray[i]->Start();
+    }
+    
+}
